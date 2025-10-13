3bfad7e96190828b2760a169f592713dfb995cf9
Suwaphat Watthakicharon <watthakicharoen_s@su.ac.th>
2025-10-13 17:53:36 +0000

    bookstoreapi

--- week9-lab2/myBookstore102/src/App.js
+++ week9-lab2/myBookstore102/src/App.js
@@ -1,32 +1,22 @@
-import React, { useEffect } from 'react';
-import { Routes, Route, useLocation } from 'react-router-dom';
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import NotFound from './components/NotFound';
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import NotFound from './components/NotFound'
 
-import HomePage from './pages/HomePage';
-import BookListPage from './pages/BookListPage';
-import BookDetailPage from './pages/BookDetailPage';
-// import EditBookPage from './pages/EditBookPage'; // เพิ่มหน้าแก้ไข
-import CategoryPage from './pages/CategoryPage';
-import AboutPage from './pages/AboutPage';
-import ContactPage from './pages/ContactPage';
-import './index.css';
+import HomePage from './pages/HomePage'
+import BookListPage from './pages/BookListPage'
+import BookDetailPage from './pages/BookDetailPage'
+import CategoryPage from './pages/CategoryPage'
+import AboutPage from './pages/AboutPage'
+import ContactPage from './pages/ContactPage'
+import './index.css'
 
-// Scroll restoration component
-function ScrollToTop() {
-  const { pathname } = useLocation();
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-  return null;
-}
 
-function App() {
+export default function App() {
   return (
     <div className="flex flex-col min-h-screen">
-      <ScrollToTop />
       <Navbar />
 
       <main className="flex-grow bg-gray-50">
@@ -34,7 +24,6 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/books" element={<BookListPage />} />
           <Route path="/books/:id" element={<BookDetailPage />} />
-          {/* <Route path="/books/:id/edit" element={<EditBookPage />} /> เพิ่ม Route แก้ไข */}
           <Route path="/categories" element={<CategoryPage />} />
           <Route path="/categories/:category" element={<CategoryPage />} />
           <Route path="/about" element={<AboutPage />} />
@@ -45,7 +34,5 @@ function App() {
 
       <Footer />
     </div>
-  );
+  )
 }
-
-export default App;
